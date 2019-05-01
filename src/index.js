554c7b8203890ec3d088651754c17ae60c97cc3c
alexander ames <alexanderthomasames@gmail.com>
2019-05-01 13:05:53 -0600

    link graphql api to apollo-client

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3000/graphql'
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
-);
+)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. About service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
